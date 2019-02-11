76a7b8fe901d9b8ff6330b6d87810780388cff55
Ivan Stoyanov <super.ludiq@gmail.com>
2019-02-11 12:29:25 +0100

    Create js-operators.js

---
+++ js-operators.js
@@ -0,0 +1,25 @@
+var now = 2016;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+// 2016 - 52
+// 1964
+
+console.log(birthYear);
+
+var ageIvan = 30;
+var agePeter = 30;
+
+ageIvan = agePeter = (3 + 5) * 4 - 6;
+// ageIvan = agePeter = 26
+//agePeter = 26
+//ageIvan = 26
+
+ageIvan++;
+ageIvan = ageIvan + 1;
+
+agePeter *= 2;
+agePeter = agePeter * 2;
+
+console.log(ageIvan);
+console.log(agePeter);
